181b2b9a990fdbcb354e52559f8ae83905803834
Ruslan Voloshin <rebisall@gmail.com>
2015-03-14 18:30:04 +0200

    updated staging deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,8 +2,11 @@ role :app, %w{deploy@prologic.com.ua}
 role :web, %w{deploy@prologic.com.ua}
 role :db, %w{deploy@prologic.com.ua}
 
-set :application, 'staging'
-set :branch, 'develop'
+# set :application, 'staging'
+# set :branch, 'develop'
+
+set :application, 'production'
+set :branch, 'master'
 #set :deploy_to, "/var/www/fshop/#{fetch(:application)}"
 
 server 'prologic.com.ua',
